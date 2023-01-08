86c2f82d998932955a80e9059b24123eca8b0101
eugenetedkim <eugenetedkim@gmail.com>
2023-01-07 20:00:45 -0800

    Add completed code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
     <div class="container">
-      <AddTodo/>
-      <Todos/>
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos.vue";
-import AddTodo from "./components/AddTodo.vue";
-
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
+import FilterTodos from './components/FilterTodos.vue';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Todos,
-    AddTodo
-  }
-}
+    AddTodo,
+    FilterTodos,
+  },
+};
 </script>
 
 <style>
 body {
-  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   line-height: 1.6;
   background: #e8f7f0;
 }
